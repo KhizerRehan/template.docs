863843db39eb5c9cd1298ee4c74ae6a8c4640a02
Khizer Rehan <khizerrehan92@gmail.com>
2024-03-17 03:29:05 +0500

    Update config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
+console.log("process.env.NODE_ENV:", process.env.NODE_ENV);
+
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
-  // Add basePath
-  basePath: "/template.docs",
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 });
 
 module.exports = withNextra();
