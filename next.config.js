a87008dfd8a2162846daf21273f8db03a9b53baf
Khizer Rehan <khizerrehan92@gmail.com>
2024-03-17 03:11:41 +0500

    Update config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   output: "export",
-  images: {
-    unoptimized: true,
-  },
+  reactStrictMode: true,
 });
 
 module.exports = withNextra();
